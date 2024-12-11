50e9fc8e6e11b26532d54b2ad275518946e1646d
Soumya Dhakad <soumyadhakad123@gmail.com>
2024-12-11 23:41:50 +0530

    Update Feedback.js

--- backend/models/Feedback.js
+++ backend/models/Feedback.js
@@ -1,46 +1,20 @@
 const mongoose = require('mongoose');
 
-const usageReportSchema = new mongoose.Schema({
+const feedbackSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
   },
-  monthlyUsage: [
-        {
-          month: {
-                type: String,
-                enum: [
-                      'January', 'February', 'March', 'April', 'May', 'June', 
-                      'July', 'August', 'September', 'October', 'November', 'December'
-                ],
-                required: true
-          },
-          usage: {
-                type: Number,
-                min: 0,
-                required: true
-          }
-        }
-  ],
-  yearlyUsage: [
-        {
-          year: {
-                type: Number,
-                required: true
-          },
-          usage: {
-                type: Number,
-                min: 0,
-                required: true
-          }
-        }
-  ],
-  updatedAt: {
-        type: Date,
-        default: Date.now
+  message: {
+      type: String,
+      required: true
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now
   }
 });
 
-module.exports = mongoose.model('UsageReport', usageReportSchema);
+module.exports = mongoose.model('Feedback', feedbackSchema);
