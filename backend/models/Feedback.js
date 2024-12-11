4d21d659e840cd455723fa28572d12eacc7a53c1
Soumya Dhakad <soumyadhakad123@gmail.com>
2024-12-11 23:21:48 +0530

    Create Feedback.js

---
+++ backend/models/Feedback.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const FeedbackSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    message: { type: String },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Feedback', FeedbackSchema);
